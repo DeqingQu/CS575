342f20f52cbe8b85970f430ed969dab681c65ec2
DeqingQu <qu.deqing@gmail.com>
2018-05-15 22:15:49 -0700

    add pest agent

--- project4/project.cpp
+++ project4/project.cpp
@@ -12,12 +12,14 @@ float NowTemp;          // temperature this month
 float NowHeight;        // grain height in inches
 int   NowNumDeer;       // current deer population
 int   NowNumWolf;     // current wolf population
+float NowNumPest;      // current pest population
 
 const float GRAIN_GROWS_PER_MONTH =             8.0;
 const int   DEER_GROWS_PER_MONTH =              20;
-const int   WOLF_GROWS_PER_MONTH =              1;
+const int   WOLF_GROWS_PER_MONTH =               1;
+const float PEST_GROWS_PER_MONTH =              1.0;
 
-const float ONE_DEER_EATS_PER_MONTH =           0.01;
+const float ONE_DEER_EATS_PER_MONTH =           0.05;
 const float ONE_WOLF_EATS_PER_MONTH =           1;
 
 const float AVG_PRECIP_PER_MONTH =              6.0;
@@ -44,7 +46,7 @@ void GrainGrowth()
     float precipFactor = exp(-pow((NowPrecip - MIDPRECIP) / 10., 2));
     float tempNowHeight  = NowHeight;
     tempNowHeight += tempFactor * precipFactor * GRAIN_GROWS_PER_MONTH;
-    tempNowHeight -= (float)NowNumDeer * ONE_DEER_EATS_PER_MONTH;
+    tempNowHeight -= (float)NowNumDeer * ONE_DEER_EATS_PER_MONTH + NowNumPest * PEST_GROWS_PER_MONTH;
     if(tempNowHeight <= 0.)
         tempNowHeight = 0.;
 #pragma omp barrier
@@ -52,46 +54,46 @@ void GrainGrowth()
     NowHeight = tempNowHeight;
 #pragma omp barrier
 #pragma omp barrier
-
 }
 
 void Deer()
 {
+    //  compute into tmp variables
     int tempNowNumDeer = NowNumDeer;
-    float grainFactor = exp(-pow((NowHeight - GRAIN_GROWS_PER_MONTH) / 10., 2));
-    unsigned int seed = time(NULL);
+    float grainFactor = 1 - exp(-pow((NowHeight - 0.0) / 10., 2));
+//    printf("grain factor = %f\n", grainFactor);
     tempNowNumDeer += grainFactor * DEER_GROWS_PER_MONTH;
-    tempNowNumDeer -= ONE_WOLF_EATS_PER_MONTH * NowNumWolf;
-    if (tempNowNumDeer <= 0)
-    {
-        tempNowNumDeer = grainFactor * DEER_GROWS_PER_MONTH;
-    }
+    if (tempNowNumDeer > ONE_WOLF_EATS_PER_MONTH * NowNumWolf && tempNowNumDeer > NowHeight / ONE_DEER_EATS_PER_MONTH)
+        tempNowNumDeer -= ONE_WOLF_EATS_PER_MONTH * NowNumWolf;
+    else
+        tempNowNumDeer = tempNowNumDeer * 0.75;
 #pragma omp barrier
+    //  copy into global variables
     NowNumDeer = tempNowNumDeer;
 #pragma omp barrier
 #pragma omp barrier
-    
 }
 
 void Wolf()
 {
     int tempNumWolf = NowNumWolf;
-    unsigned int seed = time(NULL);
-    float rand = Ranf(0., 1.0, &seed);
-    int rand_i = rand_r(&seed);
-    printf("ran = %f, rand_i = %d\n", rand, rand_i);
-    if (rand < 0.1) {
+    float deerFactor = 1 - exp(-pow((NowNumDeer - 30) / 10., 2));
+    if (deerFactor > 0.5)
         tempNumWolf += WOLF_GROWS_PER_MONTH;
-    }
-    if (rand > 0.95) {
-        tempNumWolf -= WOLF_GROWS_PER_MONTH;
-    }
-    if (tempNumWolf <= 0)
-    {
-        tempNumWolf = WOLF_GROWS_PER_MONTH;
-    }
+    if (tempNumWolf > NowNumDeer / ONE_WOLF_EATS_PER_MONTH / 6.)
+        tempNumWolf = tempNumWolf * 0.5;
+    
 #pragma omp barrier
     NowNumWolf = tempNumWolf;
+#pragma omp barrier
+#pragma omp barrier
+}
+
+void Pest()
+{
+    float tempNumPest = 1. * pow(2.71828f, -(pow((NowMonth % 12) - 6, 2.)));
+#pragma omp barrier
+    NowNumPest = tempNumPest;
 #pragma omp barrier
 #pragma omp barrier
 }
@@ -102,7 +104,7 @@ void Watcher()
     
     #pragma omp barrier
 //    printf("Month %d, Year %d\n", NowMonth % 12 + 1, NowYear);
-    printf("%f\t%f\t%f\t%d\t%d\n", NowTemp, NowPrecip, NowHeight, NowNumDeer, NowNumWolf);
+    printf("%d\t%f\t%f\t%f\t%d\t%d\n", NowMonth+1, NowTemp, NowPrecip, NowHeight, NowNumDeer, NowNumWolf);
     
     //  update month and year
     NowMonth++;
@@ -124,7 +126,7 @@ void Watcher()
 int main( )
 {
     //  setup global variables
-    NowNumDeer = 30;
+    NowNumDeer = 40;
     NowNumWolf = 4;
     NowHeight =  1.;
     NowMonth =    0;
@@ -143,7 +145,7 @@ int main( )
     return 1;
 #endif
     
-    omp_set_num_threads(4);
+    omp_set_num_threads(5);
     
     while (NowMonth < 72) {
 #pragma omp parallel sections
@@ -155,6 +157,10 @@ int main( )
 #pragma omp section
             {
                 Wolf();
+            }
+#pragma omp section
+            {
+                Pest();
             }
 #pragma omp section
             {
