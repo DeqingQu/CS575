b9a8a2d7ba8f11e70602b3815c7ab088ac9be653
Deqing Qu <qud@rabbit.engr.oregonstate.edu>
2018-05-17 22:16:34 -0700

    modify data size

--- project5/script.sh
+++ project5/script.sh
@@ -5,11 +5,11 @@ for t in 1 2 4
 do
     echo NUMT = $t
     # number of subdivisions:
-    for s in 1000 100000 32000000 64000000
+    for s in 1000 10000 100000 16000000 32000000 48000000 64000000
     do
         echo NUMS = $s
 #        gcc-7 -DNUMS=$s -DNUMT=$t project.cpp -o pj.exe -lm -fopenmp -O0 -lstdc++
-	icpc -DNUMS=$s -DNUMT=$t -o pj.exe project.cpp -lm -openmp -align -qopt-report=3 -qopt-report-file="report-"$t"-"$s"-novec.optrpt" -qopt-report-phase=vec -no-vec
+	icpc -DNUMS=$s -DNUMT=$t -o pj.exe project.cpp -lm -openmp -align -qopt-report=3 -qopt-report-file="report-"$t"-"$s".optrpt" -qopt-report-phase=vec
         ./pj.exe
     done
 done
