a2aaf148c7a41a767e42ea1e8a738a0704cd5757
Deqing Qu <qud@rabbit.engr.oregonstate.edu>
2018-05-28 20:31:57 -0700

    p6 first

--- project6/script.sh
+++ project6/script.sh
@@ -3,12 +3,11 @@
 # number of threads
 for gs in 1024 16*1024 256*1024 1024*1024 4*1024*1024 8*1024*1024
 do
-    echo GlobalSize = $gs
     # number of subdivisions:
     for ls in 1 4 16 32 128 512 1024
     do
-        echo LocalSize = $ls
-        g++ -DNUM_ELEMENTS=$gs -DLOCAL_SIZE=$ls -DNUM_WORK_GROUPS $gs/$ls -o first first.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
+	let ng=$gs/$ls
+        g++ -DNUM_ELEMENTS=$gs -DLOCAL_SIZE=$ls -DNUM_WORK_GROUPS=$ng -o first first.cpp /scratch/cuda-7.0/lib64/libOpenCL.so -lm -fopenmp
         ./first
     done
 done
