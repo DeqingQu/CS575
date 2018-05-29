752c9aeb15850d0616603ca23d5da851b7152eb6
DeqingQu <qu.deqing@gmail.com>
2018-05-28 21:31:31 -0700

    modify global size

--- project6/script-first.sh
+++ project6/script-first.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # number of threads
-for gs in 1024 16*1024 256*1024 1024*1024 4*1024*1024 8*1024*1024
+for gs in 1024 512*1024 1024*1024 2*1024*1024 4*1024*1024 8*1024*1024
 do
     # number of subdivisions:
     for ls in 1 4 16 32 128 512 1024
