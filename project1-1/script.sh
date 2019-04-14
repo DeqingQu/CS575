a1002fd7b89f6004aec4a21e6f86628f4f3832ef
DeqingQu <qu.deqing@gmail.com>
2019-04-14 16:09:20 -0700

    modify parameters

--- project1-1/script.sh
+++ project1-1/script.sh
@@ -4,9 +4,9 @@
 for t in 1 2 4 8
 do
     echo NUMT = $t
-    for a in 1000000 5000000 10000000 10000000
+    for a in 1000000 3000000 6000000 10000000
     do
-        g++ -DNUMT=$t -DNUMTRIALS=$a  project.cpp -o proj.exe -lm -fopenmp
+        g++ -DNUMT=$t -DNUMTRIALS=$a  project1.cpp -o proj.exe -lm -fopenmp
         ./proj.exe
     done
 done
