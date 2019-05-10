4d126bccde96e6f89e2bf1eaa5441212412c06cd
DeqingQu <qu.deqing@gmail.com>
2019-05-09 23:00:11 -0700

    array size

--- project4-1/script.sh
+++ project4-1/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # number of threads
-for size in 1000 10000 50000 100000 200000
+for size in 1000 5000 10000 20000 40000 60000 80000 100000 120000 140000 160000 180000 200000
 do
     echo ARRAYSIZE = $size
     g++  -c  simd.p4.cpp -o simd.p4.o
