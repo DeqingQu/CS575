989de77ce0c7cf56d7811e56e1dbb7075bebab17
DeqingQu <qu.deqing@gmail.com>
2019-05-13 21:36:58 -0700

    pdf

--- project4-1/script.sh
+++ project4-1/script.sh
@@ -4,10 +4,10 @@
 for size in 1000 5000 10000 50000 100000 200000 500000 1000000 2000000 5000000
 do
     echo ARRAYSIZE = $size
-    g++  -c  simd.p4.cpp -o simd.p4.o
-    g++  -DARRAYSIZE=$size -o main.exe  main.cpp simd.p4.o  -lm  -fopenmp
-    ./main.exe
+    g++  -c simd.p4.cpp -o simd.p4.o
+    g++  -DARRAYSIZE=$size -o main  main.cpp simd.p4.o  -lm  -fopenmp
+    ./main
 done
 
 rm simd.p4.o
-rm main.exe
+rm main
